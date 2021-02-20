63429e84d7c54b59003d2d270e49c8665d98650b
alex_zhooson <861077046@qq.com>
2021-02-20 16:40:24 +0800

    fix 登录页面

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+
 import router from "./router";
 import store from "./store";
-import "./assets/styles/index.scss";
+import App from "./App.vue";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+import "./assets/styles/index.scss";
+import "element-plus/lib/theme-chalk/index.css";
+
+// Vue.config -> app.config
+// Vue.component -> app.componentVue.directive -> app.directiveVue.filter -> app.filter
+// Vue.mixin -> app.mixinVue.use -> app.use
+
+const app = createApp(App);
+// 需要高版本来支持vue3的版本
+import { ElButton, ElForm, ElFormItem, ElInput, ElMessage } from "element-plus";
+app.component(ElButton.name, ElButton);
+app.component(ElForm.name, ElForm);
+app.component(ElFormItem.name, ElFormItem);
+app.component(ElInput.name, ElInput);
+app.component(ElMessage.name, ElMessage);
+
+app.use(store);
+app.use(router);
+app.mount("#app", { username: "张沪生" });
---
+++ src/views/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="login">
+    <el-form ref="ruleForm" label-width="100px" style="width: 400px"
+      class="demo-ruleForm">
+      <div class="title">登录</div>
+      <el-form-item class="label" label="账户" prop="account">
+        <el-input v-model="account" placeholder="账户名为：admin"></el-input>
+      </el-form-item>
+      <el-form-item class="label" label="密码" prop="password">
+        <el-input v-model="password" placeholder="密码随便写"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">
+          立即创建{{ruleForm}}
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+import { reactive, ref, toRefs } from "vue";
+import { ElMessage } from "element-plus";
+export default {
+  name: "login",
+  setup(props) {
+    const ruleForm = reactive({
+      account: "",
+      password: "",
+    });
+    console.log("ruleForm", ruleForm);
+
+    // const account = ref("");
+    // const password = ref("");
+
+    const submitForm = () => {
+      const { account, password } = ruleForm;
+      if (!account) {
+        ElMessage.error("请输入账户");
+        return;
+      } else if (!password) {
+        ElMessage.error("请输入密码");
+        return;
+      } else {
+        if (account === "admin" && password) {
+          ElMessage.success("登录成功");
+        } else {
+          ElMessage.error("账户密码错误");
+        }
+      }
+    };
+
+    return {
+      ...toRefs(ruleForm),
+      submitForm,
+      // account,
+      // password,
+    };
+  },
+};
+</script>
+<style lang="scss">
+.login {
+  height: 100vh;
+  background: #344a5f;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .title {
+    font-size: 30px;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .label /deep/ .el-form-item__label {
+    color: #fff;
+  }
+}
+</style>
